20905b5a4ed6f49ae929bd4a41b9b15783f0662f
shehab ashraf <77567693+shehhab@users.noreply.github.com>
2024-03-07 15:21:15 +0200

    Update LoginResource.php

--- app/Http/Resources/Auth/LoginResource.php
+++ app/Http/Resources/Auth/LoginResource.php
@@ -14,16 +14,16 @@ class LoginResource extends JsonResource
 
         $defaultImage = asset('Default/profile.jpeg');
         return [
-            "id" => $this->whenHas('id'),
-            "uuid" => $this->whenHas('uuid'),
-            'email_verfied'=>(bool) $this->email_verified_at ? true:false,
+            //"id" => $this->whenHas('id'),
+            //"uuid" => $this->whenHas('uuid'),
+            //'email_verfied'=>(bool) $this->email_verified_at ? true:false,
             'token' => $this->createToken('auth_token')->plainTextToken,
             "name" =>(string) $this->whenHas('name'),
-            "email" => (string) $this->whenHas('email'),
-            "date_birth" => $dateOfBirth,
+            //"email" => (string) $this->whenHas('email'),
+            //"date_birth" => $dateOfBirth,
 
-            'image'=> (string) $this->getFirstMediaUrl('seeker_profile_image')?:$defaultImage,
-            "role" => $this->when($this->hasRole('seekre'), 'seeker', 'seeker'),
+            //'image'=> (string) $this->getFirstMediaUrl('seeker_profile_image')?:$defaultImage,
+            //"role" => $this->when($this->hasRole('seekre'), 'seeker', 'seeker'),
         ];
     }
 }
